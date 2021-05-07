6c47ce840f06fac45dd88d7b80b6ca124b582176
pablogb83 <66219692+pablogb83@users.noreply.github.com>
2021-05-06 21:04:32 -0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ListaHospitalesComponent} from './lista-hospitales/lista-hospitales.component'
-const routes: Routes = [{path: '', children:[{ path: 'hospitales', component: ListaHospitalesComponent},]}];
+import { ListaHospitalesComponent } from './lista-hospitales/lista-hospitales.component';
+import { HospitalesService } from './_services/hospitales.service';
+
+const routes: Routes = [
+  {path: "hospitales", component: ListaHospitalesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [HospitalesService]
 })
 export class AppRoutingModule { }
